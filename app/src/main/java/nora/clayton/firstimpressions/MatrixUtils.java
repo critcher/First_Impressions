9f48267d6f06e12ea0f1707029b346ca99d0cf9c
Clayton Ritcher <claytonritcher@gmail.com>
2014-07-29 19:33:33 -0400

    Added an import function to the matrix utilities to import matrices from a txt file.

--- app/src/main/java/nora/clayton/firstimpressions/MatrixUtils.java
+++ app/src/main/java/nora/clayton/firstimpressions/MatrixUtils.java
@@ -1,5 +1,16 @@
 package nora.clayton.firstimpressions;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import Jama.Matrix;
 
 /**
@@ -7,6 +18,9 @@ import Jama.Matrix;
  */
 public class MatrixUtils {
     static String str(Matrix m, int decimals){
+        if(m == null){
+            return "is null";
+        }
         String decString = "";
         if(decimals != -1){
             decString = "." + decimals;
@@ -33,5 +47,55 @@ public class MatrixUtils {
 
     public static String str(Matrix m){
         return str(m, -1);
+    }
+
+    public static Matrix importMatrix(String fileName, Context c){
+        AssetManager assetManager = c.getAssets();
+        int height = 0;
+        int width = 0;
+        double[] nums;
+        try {
+            InputStream stream = assetManager.open(fileName);
+            //get the number of rows
+            stream.skip(3);
+            height = parseNum(stream).intValue();
+            stream.skip(1);
+            width = parseNum(stream).intValue();
+            nums = new double[width * height];
+            stream.skip(2);
+            Double nextNum = null;
+            int index = 0;
+            while((nextNum = parseNum(stream)) != null){
+                nums[index++] = nextNum.doubleValue();
+            }
+        }
+        catch (Exception e) {
+            Log.e("import matrix", e.toString());
+            return null;
+        }
+        return new Matrix(nums, width).transpose();
+    }
+
+    private static Double parseNum(InputStream stream){
+        String currentNum = "";
+        char nextChar;
+        try {
+            while (((nextChar = (char) stream.read()) >=  '0' && nextChar <= '9') || nextChar == '-' || nextChar == '.' || nextChar == '\n' || nextChar == 'e') {
+                if(nextChar != '\n') {
+                    currentNum += nextChar;
+                }
+            }
+        }
+        catch (IOException e){
+            return null;
+        }
+        if(currentNum == ""){
+            return null;
+        }
+        if(currentNum.contains("e")){
+            String[] parts = currentNum.split("e");
+            return new Double(new Double(parts[0]) * (Math.pow(10,new Double(parts[1]))));
+        }
+        return new Double(currentNum);
     }
 }
